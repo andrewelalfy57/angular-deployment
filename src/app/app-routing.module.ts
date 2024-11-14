f0c514ea7eb4da7539a8ea95b71abdbd55023b7d
andrewelalfy57 <andrewabotwila@gmail.com>
2024-11-14 16:16:30 +0200

    Deploy Angular app to GitHub Pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { routes } from './app.routes';
+import { routes } from './app.routes'; // Assuming you have a separate file for routes
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy } // Use HashLocationStrategy
+  ]
 })
 export class AppRoutingModule {}
